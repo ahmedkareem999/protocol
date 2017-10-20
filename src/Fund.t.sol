594ae829345f9dde721271b3e60e905f1b1e2281
Travis Jacobs <trav221@gmail.com>
2017-10-20 14:52:00 +0200

    Update ds-tests with new datafeed parameters

--- src/Fund.t.sol
+++ src/Fund.t.sol
@@ -10,7 +10,7 @@ import "./riskmgmt/RiskMgmt.sol";
 import "./Fund.sol";
 
 
-contract DataFeedTest is DSTest {
+contract FundTest is DSTest {
     DataFeed datafeed;
     EtherToken etherToken;
     Fund fund;
@@ -29,6 +29,13 @@ contract DataFeedTest is DSTest {
     uint MANAGEMENT_REWARD = 0;
     uint PERFORMANCE_REWARD = 0;
     address MANAGER_ADDRESS = this;
+    string MELON_NAME = "Melon Token";
+    string MELON_SYMBOL = "MLN-T";
+    string MELON_URL = "https://www.melonport.com";
+    bytes32 MOCK_IPFS_HASH = 0x86b5eed81db5f691c36cc83eb58cb5205bd2090bf3763a19f0c5bf2f074dd84b;
+    bytes32 MOCK_CHAIN_ID = 0xd8344c361317e3736173f8da91dec3ca1de32f3cc0a895fd6363fbc20fd21985;
+    address MOCK_BREAK_IN = 0x2186C5EaAf6CbF55BF1b9cD8130D8a6A71E4486a;
+    address MOCK_BREAK_OUT = 0xd9AE70149D256D4645c03aB9D5423A1B70d8804d;
 
     // mock data
     uint inputEtherTokenPrice = 4152823920265781000;
@@ -36,10 +43,10 @@ contract DataFeedTest is DSTest {
     uint mockQuantity = 1 ether;
 
     function setUp() {
-        melonToken = new PreminedAsset("Melon Token", "MLN-T", MELON_DECIMALS, PREMINED_AMOUNT);
+        melonToken = new PreminedAsset(MELON_NAME, MELON_SYMBOL, MELON_DECIMALS, PREMINED_AMOUNT);
         etherToken = new EtherToken();
-        datafeed = new DataFeed(melonToken, INTERVAL, VALIDITY);
         simpleMarket = new SimpleMarket();
+        datafeed = new DataFeed(melonToken, MELON_NAME, MELON_SYMBOL, MELON_DECIMALS, MELON_URL, MOCK_IPFS_HASH, MOCK_CHAIN_ID, MOCK_BREAK_IN, MOCK_BREAK_OUT, INTERVAL, VALIDITY);
         sphere = new Sphere(datafeed, simpleMarket);
         participation = new Participation();
         fund = new Fund(
--- src/version/Version.t.sol
+++ src/version/Version.t.sol
@@ -32,15 +32,22 @@ contract VersionTest is DSTest {
     uint MANAGEMENT_REWARD = 0;
     uint PERFORMANCE_REWARD = 0;
     address MANAGER_ADDRESS = this;
+    string MELON_NAME = "Melon Token";
+    string MELON_SYMBOL = "MLN-T";
+    string MELON_URL = "https://www.melonport.com";
+    bytes32 MOCK_IPFS_HASH = 0x86b5eed81db5f691c36cc83eb58cb5205bd2090bf3763a19f0c5bf2f074dd84b;
+    bytes32 MOCK_CHAIN_ID = 0xd8344c361317e3736173f8da91dec3ca1de32f3cc0a895fd6363fbc20fd21985;
+    address MOCK_BREAK_IN = 0x2186C5EaAf6CbF55BF1b9cD8130D8a6A71E4486a;
+    address MOCK_BREAK_OUT = 0xd9AE70149D256D4645c03aB9D5423A1B70d8804d;
 
 
 //TODO: uncomment these tests when ds-test issue is resolved:
 //      https://github.com/dapphub/ds-test/issues/6
     function setUp() {
         governance = new Governance();
-        melonToken = new PreminedAsset("Melon Token", "MLN-T", MELON_DECIMALS, PREMINED_AMOUNT);
+        melonToken = new PreminedAsset(MELON_NAME, MELON_SYMBOL, MELON_DECIMALS, PREMINED_AMOUNT);
         version = new Version(VERSION_NUMBER, governance, melonToken);
-        datafeed = new DataFeed(melonToken, INTERVAL, VALIDITY);
+        datafeed = new DataFeed(melonToken, MELON_NAME, MELON_SYMBOL, MELON_DECIMALS, MELON_URL, MOCK_IPFS_HASH, MOCK_CHAIN_ID, MOCK_BREAK_IN, MOCK_BREAK_OUT, INTERVAL, VALIDITY);
         riskMgmt = new RiskMgmt();
         simpleMarket = new SimpleMarket();
         sphere = new Sphere(datafeed, simpleMarket);
